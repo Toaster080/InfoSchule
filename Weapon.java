925bb307211f982d51353b67711e3a38a29dbd71
Toaster080 <158219297+Toaster080@users.noreply.github.com>
2025-07-02 11:25:18 +0200

    Weapon.java aktualisieren

--- Weapon.java
+++ Weapon.java
@@ -1,1 +1,15 @@
+public class Weapon {
+    private final WeaponType type;
 
+    public Weapon(WeaponType type) {
+        this.type = type;
+    }
+
+    public int getDamage() {
+        return type.getDamage();
+    }
+
+    public String getName() {
+        return type.getName();
+    }
+}
